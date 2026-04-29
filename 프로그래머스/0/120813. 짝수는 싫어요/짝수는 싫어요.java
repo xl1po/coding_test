1f510158ec180e204447c5d372f9d3a763000e8e
김선민 <shanekim0519@gmail.com>
2026-04-29 13:50:56 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        if(n%2 == 0) answer = new int[n/2];
+        else answer = new int[n/2 + 1];
+        
+        int odd = 1;
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = odd;
+            odd += 2;
+        }
+        
+        return answer;
+    }
+}
