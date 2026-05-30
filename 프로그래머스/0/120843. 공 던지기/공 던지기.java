e6ac5a5479c1dba1e51800b09079e25171d9b0b3
김선민 <shanekim0519@gmail.com>
2026-05-30 22:21:43 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        
+        int index = ((k-1)*2)%numbers.length;
+        answer = numbers[index];
+        
+        return answer;
+    }
+}
