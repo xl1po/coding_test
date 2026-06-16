9495cc5cc347cdd0853eb793f16b9c93f21d6baf
김선민 <shanekim0519@gmail.com>
2026-06-16 11:41:19 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        for(int i=0; i<num_list.length - n; i++) {
+            answer[i] = num_list[i+n];
+        }
+        for(int i=0; i<n; i++) {
+            answer[i+num_list.length-n] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
