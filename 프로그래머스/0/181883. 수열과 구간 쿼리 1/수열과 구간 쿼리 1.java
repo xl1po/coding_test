001fd7e24e52e4e6cc1195ba6d34b7fd644a0108
김선민 <shanekim0519@gmail.com>
2026-06-23 11:58:29 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.34 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i=0; i<queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            for(int j=s; j<=e; j++) {
+                arr[j]++;
+            }
+        }
+        
+        int[] answer = arr;
+        return answer;
+    }
+}
