d093dd0aa2b0d3b019053a53bd292809f01f1a1a
김선민 <shanekim0519@gmail.com>
2026-06-28 21:30:53 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        for(int i=0; i<num_list.length; i++) {
+            for(; num_list[i] != 1; ) {
+                if(num_list[i]%2 == 0) {
+                    num_list[i] /= 2;
+                } else {
+                    num_list[i] -= 1;
+                    num_list[i] /= 2;
+                }
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
