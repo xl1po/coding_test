4eb6491c36ab258170014ab430de5788d8bf1c10
김선민 <shanekim0519@gmail.com>
2026-07-19 20:46:12 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(; n > 0; n/=10) {
+            answer += n%10;
+        }
+        
+        return answer;
+    }
+}
