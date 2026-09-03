71154bb43d0d10e73691eb57c1bdc651502d3942
김선민 <shanekim0519@gmail.com>
2026-09-03 19:08:28 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        
+        for(int i=2; i<=a; i++) {
+            if(a%i == 0 && b%i == 0) {
+                a/=i;
+                b/=i;
+                i=2;
+            }
+        }
+        for(int i=2; i<=b; i++) {
+            if(!(i%2==0 || i%5==0) && b%i == 0) answer = 2;
+        }
+        
+        return answer;
+    }
+}
