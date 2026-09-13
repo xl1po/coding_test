0c5186636d48b5c8fde5d7423e69f0742b6bbb30
김선민 <shanekim0519@gmail.com>
2026-09-13 22:50:36 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int result = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        String answer = Integer.toBinaryString(result);
+        
+        return answer;
+    }
+}
